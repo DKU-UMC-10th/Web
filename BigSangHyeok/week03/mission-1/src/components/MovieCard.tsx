a480c54dc4426627f2f4a7b9260d8cee4b028840
이상혁 <32203155@dankook.ac.kr>
2026-04-04 23:04:13 +0900

    mission: week03-3 구현

--- BigSangHyeok/week03/mission-1/src/components/MovieCard.tsx
+++ BigSangHyeok/week03/mission-1/src/components/MovieCard.tsx
@@ -11,7 +11,7 @@ export default function MovieCard({ movie } : MovieCardProps) {
     const navigate = useNavigate();
 
     return <div 
-                onClick = {() => navigate(`/movies/${movie.id}`)}
+                onClick = {() => navigate(`/movie/${movie.id}`)}
                 className='relative rounded-xl shadow-lg overflow-hidden cursor-pointer
                 w-44 transition-transform duration-300 hover:scale-105' 
                 onMouseEnter={() => setIsHovered(true)}
