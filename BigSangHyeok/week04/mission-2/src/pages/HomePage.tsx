9538b0af7df22b28a5d6624fb9b71e2d81345503
이상혁 <32203155@dankook.ac.kr>
2026-04-06 02:09:59 +0900

    mission: week04-2 구현

---
+++ BigSangHyeok/week04/mission-2/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <section className="min-h-[calc(100dvh-4rem)] flex items-center justify-center px-4">
+            <h1 className="text-4xl md:text-5xl font-bold tracking-tight text-center">대상혁</h1>
+        </section>
+    );
+};
+
+export default HomePage;
