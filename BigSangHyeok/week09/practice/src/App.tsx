5c0a5ddcea6c44f866ce775be7f97117cc98e85e
이상혁 <32203155@dankook.ac.kr>
2026-05-27 01:04:32 +0900

    practice: week09 구현

---
+++ BigSangHyeok/week09/practice/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import UseReducerCompany from './useReducer/useReducerCompany';
+
+
+export default function App() {
+  return (
+    <>
+      <UseReducerCompany />
+    </>
+  );
+}
